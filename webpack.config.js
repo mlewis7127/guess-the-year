84860bb642c08d75acaff0f62def69de0d193dd4
Matthew Lewis <matthew.lewis@dvla.gov.uk>
2019-08-30 12:10:12 +0100

    Add webpack (but think its wrong)

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    entry: {
+      'functions/index': './functions/index.js',
+      'functions/data/getNextQuestion': './functions/data/get-question.js',
+    },
+    mode: 'production',
+    target: 'node'
+  }
